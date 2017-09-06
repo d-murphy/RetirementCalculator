7c1b71c10bcc457c39924c57846eff7af7fc5d06
d-murphy <murphy.dan.s@gmail.com>
2017-09-05 21:24:59 -0400

    After UI update

--- server.R
+++ server.R
@@ -1,64 +1,69 @@
-
 library(ggplot2)
 library(dplyr)
+library(ggthemes)
 
 
 
 library(shiny)
 
 shinyServer(function(input, output) {
-
-    df <- reactive({
-      
-      df <- data.frame(Age = 17:101,
-                 Total = 0, 
-                 TotalWAddInv = 0,
-                 Addition = 0)
-      
-      df$Addition <- ifelse(df$Age >= input$YearInv[1] &
-                            df$Age <= input$YearInv[2], input$YearlyInv, 0)
-      
-      for(i in 1:84){
-        
-        df$Total[i+1] <- df$Total[i] * (100 + input$IntRate) / 100 + df$Addition[i] +
-                                   if(input$InitInvAge==df$Age[i]){input$InInv}else{0}
-        
-        df$TotalWAddInv[i+1] <- df$TotalWAddInv[i] * (100 + input$IntRate) / 100 + df$Addition[i] + 
-                                   if(input$InitInvAge==df$Age[i]){input$InInv}else{0} +
-                                   if(input$OneTimeInvAge==df$Age[i]){input$OneTimeInv}else{0} 
-        
-      }
-      
-      df$Total4Pct <- df$Total * .04
-      df$TotalWAddInv4Pct <- df$TotalWAddInv * .04
-      
-      return(df)
-      
-    })
-
-    output$InvTotal <- renderPlot({
-      
-      ggplot(df(), aes(x=Age)) + 
-        geom_line(aes(y=Total),color = "red") + 
-        geom_line(aes(y=TotalWAddInv),color = "blue") +
-        scale_y_continuous(labels = scales::comma, limits = c(0,10000000)) + 
-        scale_x_continuous(breaks = c(18,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100))
-      
-    })
-      
-     output$Inv4Pct <- renderPlot({
-        
-        ggplot(df(), aes(x=Age)) + 
-          geom_line(aes(y=Total4Pct),color = "red") + 
-          geom_line(aes(y=TotalWAddInv4Pct),color = "blue") +
-          scale_y_continuous(labels = scales::comma, limits = c(0,200000)) + 
-          scale_x_continuous(breaks = c(18,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100))
-      
-      
-      
-    })
+  
+  df <- reactive({
     
+    df <- data.frame(Age = 17:70,
+                     Total = 0, 
+                     TotalWAddInv = 0,
+                     Addition = 0)
+    
+    df$Addition <- ifelse(df$Age >= input$YearInv[1] &
+                            df$Age <= input$YearInv[2], input$YearlyInv, 0)
+    
+    for(i in 1:53){
+      
+      df$Total[i+1] <- df$Total[i] * (100 + input$IntRate - if(input$WdAge<=df$Age[i]){input$WdRate}else{0}) / 100 + 
+        df$Addition[i] +
+        if(input$InitInvAge==df$Age[i]){input$InInv}else{0}
+      
+      df$TotalWAddInv[i+1] <- df$TotalWAddInv[i] * (100 + input$IntRate - if(input$WdAge<=df$Age[i]){input$WdRate}else{0}) / 100 + 
+        df$Addition[i] + 
+        if(input$InitInvAge==df$Age[i]){input$InInv}else{0} +
+        if(input$OneTimeInvAge==df$Age[i]){input$OneTimeInv}else{0} 
+      
+    }
+    
+    df$Total4Pct <- ifelse(input$WdAge > df$Age,0,df$Total * input$WdRate / 100)
+    df$TotalWAddInv4Pct <- ifelse(input$WdAge>df$Age,0,df$TotalWAddInv * input$WdRate / 100)
+    
+    return(df)
+    
+  })
+  
+  output$InvTotal <- renderPlot({
+    
+    ggplot(df(), aes(x=Age)) + 
+      geom_line(aes(y=Total), color = "black") + 
+      geom_line(aes(y=TotalWAddInv), color = "dark blue", size = 2) +
+      scale_y_continuous(labels = scales::comma, limits = c(0,5000000)) + 
+      scale_x_continuous(breaks = c(18,25,30,35,40,45,50,55,60,65,70)) +
+      theme_minimal() + 
+      ggtitle("Portfolio Total") +
+      labs(y = "Dollars")
+    
+  })
+  
+  output$Inv4Pct <- renderPlot({
+    
+    ggplot(df(), aes(x=Age)) + 
+      geom_line(aes(y=Total4Pct), color = "black") + 
+      geom_line(aes(y=TotalWAddInv4Pct), color = "dark blue", size = 2) +
+      scale_y_continuous(labels = scales::comma, limits = c(0,5000000*.04)) + 
+      scale_x_continuous(breaks = c(18,25,30,35,40,45,50,55,60,65,70)) +
+      theme_minimal() + 
+      ggtitle("Amount Withdrawn") +
+      labs(y = "Dollars")
+    
+    
+    
+  })
+  
 })
-
-
-
--- ui.R
+++ ui.R
@@ -1,38 +1,53 @@
-
-
-
-
-
 library(shiny)
-
-shinyUI(fluidPage(
-
+library(shinythemes)
+shinyUI(fluidPage(theme = shinytheme("flatly"),
+  
   # Application title
-  titlePanel("When should I Sell My Bitcoin?"),
+  titlePanel("When should I sell my Bitcoin?"),
   p("A retirement calculator by Dan Murphy"),
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("InitInvAge", label = h5("Age"), min = 18, 
-                  max = 100, value = c(18)),
-      numericInput("InInv", label = h5("Current Investments"), value = 0), 
-      sliderInput("YearInv", label = h5("Years Investing"), min = 18, 
-                  max = 100, value = c(22, 32)), 
-      numericInput("YearlyInv", label = h5("Annual Investment"), value = 0),
-      sliderInput("OneTimeInvAge", label = h5("One-time Investment Age"), min = 18, 
-                  max = 100, value = c(18)),
-      numericInput("OneTimeInv", label = h5("One-time Investment"), value = 0),
+      fluidRow(
+        column(7,
+               sliderInput("InitInvAge", label = h5("Current Age"), min = 18, 
+                           max = 100, value = c(18), animate = TRUE)),
+        column(5,
+               numericInput("InInv", label = h5("Current Investments"), value = 0)               
+               )
+      ),
       
-      numericInput("IntRate", label = h5("Interest Rate (%)"), value = 7)
+      fluidRow(
+        column(7,
+                sliderInput("YearInv", label = h5("Years to Invest"), min = 18, 
+                  max = 100, value = c(22, 32), animate = TRUE)),
+        column(5,
+               numericInput("YearlyInv", label = h5("Annual Investment"), value = 0))
+        ),
       
+      fluidRow(
+        column(7,
+               sliderInput("OneTimeInvAge", label = h5("One-time Investment Age"), min = 18, 
+                           max = 100, value = c(18), animate = TRUE)),
+        column(5,
+               numericInput("OneTimeInv", label = h5("One-time Investment"), value = 0))
+      ),
       
+      numericInput("IntRate", label = h5("Investment Return (%)"), value = 7),
       
+      fluidRow(
+        column(7,
+               sliderInput("WdAge", label = h5("Withdrawel Age"), min = 18, 
+                           max = 100, value = c(50), animate = TRUE)),
+        column(5,
+               numericInput("WdRate", label = h5("Withdrawel Rate (%)"), value = 4))
+              )
     ),
-
+    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("InvTotal"), 
-      plotOutput("Inv4Pct")
+      plotOutput("InvTotal", height = 325), 
+      plotOutput("Inv4Pct", height = 325)
     )
   )
 ))
